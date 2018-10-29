e1e4c272ca8eac34909b503a81812cb10c554210
Silnari <44261671+Silnari@users.noreply.github.com>
2018-10-29 17:09:50 +0100

    Update MapDistance.h

--- Lab4/MapDistance.h
+++ Lab4/MapDistance.h
@@ -5,6 +5,6 @@ using namespace std;
 
 void movePoint(MapPoint &mp, double szer, double dl);
 
-MapPoint inTheMiddle(MapPoint mp1, MapPoint mp2, char text[]);
+MapPoint inTheMiddle(MapPoint mp1, MapPoint mp2, const char text[]);
 
 void clear(MapPoint &mp);
