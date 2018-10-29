8009999f3b6dc947d5fe66a6035b30fcd2ba72a3
Silnari <44261671+Silnari@users.noreply.github.com>
2018-10-29 17:09:33 +0100

    Update MapDistance.cpp

--- Lab4/MapDistance.cpp
+++ Lab4/MapDistance.cpp
@@ -6,12 +6,12 @@ void movePoint(MapPoint &mp, double szer, double dl){
 	mp.y+=dl;
 }
 
-MapPoint inTheMiddle(MapPoint mp1, MapPoint mp2, char text[]){
-	MapPoint *mp = new MapPoint;
-	mp->nazwa = text;
-	mp->x = (mp1.x+mp2.x)/2;
-	mp->y = (mp1.y+mp2.y)/2;
-	return *mp;
+MapPoint inTheMiddle(MapPoint mp1, MapPoint mp2, const char text[]){
+	MapPoint mp;
+	mp.nazwa = text;
+	mp.x = (mp1.x+mp2.x)/2;
+	mp.y = (mp1.y+mp2.y)/2;
+	return mp;
 }
 
 void clear(MapPoint &mp){
